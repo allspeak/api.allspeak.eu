19e8172f30ec04d502ba1c25ca9c4fe51358a424
federicobozzini <federico.bozzini@gmail.com>
2017-11-03 16:00:11 +0100

    Added endpoint to download a trained network.

--- web/project/training_api/views.py
+++ web/project/training_api/views.py
@@ -1,14 +1,16 @@
-from flask import render_template, Blueprint, request, redirect, url_for, abort, jsonify, g, make_response
+from flask import render_template, Blueprint, request, redirect, url_for, abort, jsonify, g, make_response, send_file
 from project import db, auth, auth_token, app, images
-import uuid, os
+import uuid
+import os
 import zipfile
 import json
+import glob
 from werkzeug import secure_filename
 from project.exceptions import RequestException
 from .libs import context, train
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
- 
+
 training_api_blueprint = Blueprint('training_api', __name__)
 
 # the root path is : /home/flask/app/web
@@ -23,19 +25,20 @@ training_api_blueprint = Blueprint('training_api', __name__)
 # - commands list
 # - modeltype (fine tune init net  OR  new net)
 
+
 @training_api_blueprint.route('/users/<int:user_id>/training-sessions', methods=['POST'])
 def add_training_session(user_id):
 
     if 'file' not in request.files or request.files['file'].filename == '':
         msg = 'ERROR: no file in request'
         raise RequestException(msg)
-    
+
     training_sessionid = uuid.uuid1()
     print(training_sessionid)
 
     # check & prepare file system
     session_path = os.path.join('project', 'data', str(training_sessionid))
-    if os.path.exists(session_path): 
+    if os.path.exists(session_path):
         msg = 'ERROR: training session %d already exist' % training_sessionid
         raise Exception(msg)
     file = request.files['file']
@@ -51,7 +54,7 @@ def add_training_session(user_id):
     with zipfile.ZipFile(file_path, "r") as zip_ref:
         zip_ref.extractall(data_path)
 
-    # copy json to ../ (data/training_sessionid), read it 
+    # copy json to ../ (data/training_sessionid), read it
     src_json_filename = os.path.join(data_path, 'training.json')
     dest_json_filename = os.path.join(session_path, 'training.json')
     os.rename(src_json_filename, dest_json_filename)
@@ -59,14 +62,18 @@ def add_training_session(user_id):
     with open(dest_json_filename, 'r') as data_file:
         train_data = json.load(data_file)
 
-    modeltype = train_data['nModelType']                    # newnet or ft_initnet
-    commands = train_data['commands']                       # list of commands id [1102, 1103, 1206, ...]
+    # newnet or ft_initnet
+    modeltype = train_data['nModelType']
+    # list of commands id [1102, 1103, 1206, ...]
+    commands = train_data['commands']
     commands_ids = [cmd['id'] for cmd in commands]
     str_proc_scheme = str(train_data['nProcessingScheme'])  # 252/253/254/255
 
     executor = ThreadPoolExecutor(2)
-    executor.submit(train.train_net, training_sessionid, user_id, modeltype, commands_ids, str_proc_scheme, True)
+    executor.submit(train.train_net, training_sessionid, user_id,
+                    modeltype, commands_ids, str_proc_scheme, True)
     return jsonify({'training_session_id': training_sessionid})
+
 
 @training_api_blueprint.route('/users/<int:user_id>/training-sessions/<session_id>', methods=['GET'])
 def get_training_session(user_id, session_id):
@@ -78,53 +85,55 @@ def get_training_session(user_id, session_id):
         res = {'status': 'pending'}
         return jsonify(res)
 
-  
     session_json_filename = os.path.join(session_path, 'training.json')
     with open(session_json_filename, 'r') as data_file:
         session_data = json.load(data_file)
 
     modeltype = session_data['nModelType']
     if modeltype == 274:
-        trainparams_json = os.path.join('project', 'training_api', 'train_params.json')    
+        trainparams_json = os.path.join(
+            'project', 'training_api', 'train_params.json')
     else:
-        trainparams_json = os.path.join('project', 'training_api', 'ft_train_params.json')
-    
+        trainparams_json = os.path.join(
+            'project', 'training_api', 'ft_train_params.json')
+
     with open(trainparams_json, 'r') as data_file:
         train_data = json.load(data_file)
 
     nitems = len(session_data['commands'])
 
-    output_net_name = "optimized_%s_%d_%d.pb" % (train_data['sModelFileName'], user_id, session_data['nProcessingScheme'])
+    output_net_name = "optimized_%s_%d_%d.pb" % (
+        train_data['sModelFileName'], user_id, session_data['nProcessingScheme'])
 
-    # create return JSON  
+    # create return JSON
     nw = datetime.now()
-    res = { 'status': 'complete',
-            'nModelType':session_data['nModelType'],
-            'nInputParams':train_data['nInputParams'],
-            'nContextFrames':train_data['nContextFrames'],
-            'nItemsToRecognize':nitems,
-            'sModelFileName':output_net_name,
-            'sInputNodeName':train_data['sInputNodeName'],
-            'sOutputNodeName':train_data['sOutputNodeName'],            
-            'sLocalFolder':session_data['sLocalFolder'],
-            'nProcessingScheme':session_data['nProcessingScheme'],
-            'sCreationTime':nw.strftime('%Y/%m/%d %H:%M:%S'),
-            'commands':session_data['commands']
-            }
+    res = {'status': 'complete',
+           'nModelType': session_data['nModelType'],
+           'nInputParams': train_data['nInputParams'],
+           'nContextFrames': train_data['nContextFrames'],
+           'nItemsToRecognize': nitems,
+           'sModelFileName': output_net_name,
+           'sInputNodeName': train_data['sInputNodeName'],
+           'sOutputNodeName': train_data['sOutputNodeName'],
+           'sLocalFolder': session_data['sLocalFolder'],
+           'nProcessingScheme': session_data['nProcessingScheme'],
+           'sCreationTime': nw.strftime('%Y/%m/%d %H:%M:%S'),
+           'commands': session_data['commands']
+           }
     return jsonify(res)
-
 
 
 @training_api_blueprint.route('/users/<int:user_id>/training-sessions/<session_id>/network', methods=['GET'])
 def get_training_session_network(user_id, session_id):
-    try:
-        filename = "user_%d_FT_net_fsc" % user_id
-        filepath = os.path.join('data', str(session_id), 'net', filename)
-        if (os.path.isfile(filepath)):
-            attachment_filename = session_id
-            return send_file(filepath, attachment_filename=attachment_filename)
-        else:
-            abort(404)
-    except Exception as e:
-        print(str(e))
+    directory_name = os.path.join(app.root_path, 'data', str(session_id))
+    if not os.path.isdir(directory_name):
+        abort(404)
+    filepaths = glob.glob(os.path.join(directory_name, '*.pb'))
+    if len(filepaths) != 1:
         abort(500)
+    filepath = filepaths[0]
+    if (os.path.isfile(filepath)):
+        attachment_filename = session_id
+        return send_file(filepath, attachment_filename=attachment_filename)
+    else:
+        abort(404)
