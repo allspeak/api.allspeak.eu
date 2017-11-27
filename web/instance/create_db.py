b6fda5d93c4bbc4a4fc0ed64a6823f1b33fd1303
federicobozzini <federico.bozzini@gmail.com>
2017-11-27 18:21:57 +0100

    Added python script to initialize the application.

---
+++ web/instance/create_db.py
@@ -0,0 +1,27 @@
+# Check the PYTHONPATH environment variable before beginning to ensure that the
+# top-level directory is included.  If not, append the top-level.  This allows
+# the modules within the .../project/ directory to be discovered.
+import sys
+import os
+
+print('Creating database tables for All Speak...')
+
+if os.path.abspath(os.curdir) not in sys.path:
+    sys.path.append(os.path.abspath(os.curdir))
+
+
+# Create the database tables, add some initial data, and commit to the database
+from project import db
+from project.models import User
+
+
+# Drop all of the existing database tables
+db.drop_all()
+
+# Create the database and the database table
+db.create_all()
+
+# Commit the changes for the users
+db.session.commit()
+
+print('...done!')
---
+++ web/instance/create_neurologist.py
@@ -0,0 +1,27 @@
+# Check the PYTHONPATH environment variable before beginning to ensure that the
+# top-level directory is included.  If not, append the top-level.  This allows
+# the modules within the .../project/ directory to be discovered.
+import sys
+import os
+
+email = sys.argv[1]
+password = sys.argv[2]
+
+print('Creating neurologist %s...' % email)
+
+if os.path.abspath(os.curdir) not in sys.path:
+    sys.path.append(os.path.abspath(os.curdir))
+
+
+# Create the database tables, add some initial data, and commit to the database
+from project import db
+from project.models import User
+
+# Insert user data
+neurologist = User(email=email, plaintext_password=password, role='neurologist')
+db.session.add(neurologist)
+
+# Commit the changes for the users
+db.session.commit()
+
+print('...done!')
